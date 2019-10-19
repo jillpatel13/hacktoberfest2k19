f0efa89d11e1b6e5eccacf3946b711edea813a40
Jill Hansalia <54848988+jillpatel13@users.noreply.github.com>
2019-10-20 00:20:54 +0530

    Update primefactorization.py

--- Prime Factors/primefactorization.py
+++ Prime Factors/primefactorization.py
@@ -1,15 +1,17 @@
 n=int(input("Enter an integer:"))
 print("Factors are:")
 i=0
-for(i=0;i<n;i++)
-while(i<=n):
-    k=0
-    if(n%i==0):
-        j=1
-        while(j<=i):
-            if(i%j==1):
-                k=k+1
-            j=j+1
-        if(k==2):
-            print(i)
-    i=i+1
+while n % 2 == 0: 
+        print 2, 
+        n = n / 2
+for i in range(3,int(math.sqrt(n))+1,2): 
+          
+        # while i divides n , print i ad divide n 
+            while n % i== 0: 
+                print i, 
+                n = n / i 
+              
+    # Condition if n is a prime 
+    # number greater than 2 
+  if n > 2: 
+      print n 
